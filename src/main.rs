0728bc4f5c7b1cc6190f0fceebb10bdbc8140e3c
Lali Micskei <micskeil@gmail.com>
2024-03-18 19:52:11 +0100

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -28,10 +28,10 @@ fn main() {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         println!("Logs from your program will appear here!");
 
-        // Uncomment this block to pass the first stage
-        // let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", decoded_value.to_string());
+        Uncomment this block to pass the first stage
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+        println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
